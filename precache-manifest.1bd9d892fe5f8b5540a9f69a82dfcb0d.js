6082e7b4b36870ff9e037a89ac434b1754ab007f
chemazu <chemazu>
2021-08-13 01:35:48 +0100

    Updates

---
+++ precache-manifest.1bd9d892fe5f8b5540a9f69a82dfcb0d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "921d4f8295e534645042",
+    "url": "/lookaxa/static/js/0.921d4f82.chunk.js"
+  },
+  {
+    "revision": "1e30d6eacbbd4b43130d",
+    "url": "/lookaxa/static/js/1.1e30d6ea.chunk.js"
+  },
+  {
+    "revision": "8dda3084064026e49d65",
+    "url": "/lookaxa/static/js/2.8dda3084.chunk.js"
+  },
+  {
+    "revision": "a598c5695e33cfb86a3f",
+    "url": "/lookaxa/static/js/3.a598c569.chunk.js"
+  },
+  {
+    "revision": "b2422fba2a5e8b6a4d05",
+    "url": "/lookaxa/static/js/4.b2422fba.chunk.js"
+  },
+  {
+    "revision": "d80e73e866d562f6f55c",
+    "url": "/lookaxa/static/js/main.d80e73e8.chunk.js"
+  },
+  {
+    "revision": "7f9f7e4c24c779d049f3",
+    "url": "/lookaxa/static/js/runtime~main.7f9f7e4c.js"
+  },
+  {
+    "revision": "9375dbfaeed5c0bcd75e",
+    "url": "/lookaxa/static/css/7.def8abe3.chunk.css"
+  },
+  {
+    "revision": "9375dbfaeed5c0bcd75e",
+    "url": "/lookaxa/static/js/7.9375dbfa.chunk.js"
+  },
+  {
+    "revision": "54da279fadbc7a1aaed3",
+    "url": "/lookaxa/static/css/8.80c80e93.chunk.css"
+  },
+  {
+    "revision": "54da279fadbc7a1aaed3",
+    "url": "/lookaxa/static/js/8.54da279f.chunk.js"
+  },
+  {
+    "revision": "6f50ec3ffa3e9a07fbe4",
+    "url": "/lookaxa/static/css/9.f8aab76c.chunk.css"
+  },
+  {
+    "revision": "6f50ec3ffa3e9a07fbe4",
+    "url": "/lookaxa/static/js/9.6f50ec3f.chunk.js"
+  },
+  {
+    "revision": "35df2a34b1e2e84e99db",
+    "url": "/lookaxa/static/js/10.35df2a34.chunk.js"
+  },
+  {
+    "revision": "cb9ce1a0cb12fce6dd7a",
+    "url": "/lookaxa/static/js/11.cb9ce1a0.chunk.js"
+  },
+  {
+    "revision": "55cbd0242b16e788adc1",
+    "url": "/lookaxa/static/js/12.55cbd024.chunk.js"
+  },
+  {
+    "revision": "225e196bdb6b1cd28762",
+    "url": "/lookaxa/static/js/13.225e196b.chunk.js"
+  },
+  {
+    "revision": "80b0776fdb06cbb76a74",
+    "url": "/lookaxa/static/js/14.80b0776f.chunk.js"
+  },
+  {
+    "revision": "2d29046ae30d53850fa378e8bb0283e7",
+    "url": "/lookaxa/index.html"
+  }
+];
